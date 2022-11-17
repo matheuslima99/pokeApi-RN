56bb02dd557ec55ccdd21a11c8b254219bc787fc
matheuslima99 <maths.soares99@gmail.com>
2022-11-17 02:49:18 -0300

    Configuring project

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,8 @@
+import { defaultTheme } from "../global/styles/themes/default";
+
+
+type ThemeType = typeof defaultTheme;
+
+declare module "styled-components" {
+  export interface DefaultTheme extends ThemeType {}
+}

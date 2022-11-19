bd95174df1e646569842b71c6078a0da74862ac5
matheuslima99 <maths.soares99@gmail.com>
2022-11-19 20:54:47 -0300

    Application features - Fetching api

--- src/Hooks/useRequest.ts
+++ src/Hooks/useRequest.ts
@@ -15,3 +15,4 @@ export function useRequest<T = unknown>(url: string) {
 
   return { data };
 }
+
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,61 @@
-import { View } from "react-native";
-import { useRequest } from "../../Hooks/useRequest";
+import { useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import { api } from "../../services/api";
+
+interface PokemonType {
+  type: string;
+}
+
+interface Pokemon {
+  id: number;
+  url: string;
+  name: string;
+  types: PokemonType[];
+}
+
+interface Request {
+  id: number;
+  types: PokemonType[];
+}
 
 export function Home() {
-  const { data } = useRequest("pokemon/");
-  console.log(data);
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
-  return <View />;
+  useEffect(() => {
+    async function getAllPokemon() {
+      const response = await api.get("/pokemon");
+      const { results } = response.data;
+
+      const payloadPokemons = await Promise.all(
+        results.map(async (pokemon: Pokemon) => {
+          const { id, types } = await getMoreInfo(pokemon.url);
+
+          return {
+            id,
+            name: pokemon.name,
+            types,
+          };
+        })
+      );
+
+      setPokemons(payloadPokemons);
+    }
+
+    getAllPokemon();
+  }, []);
+
+  async function getMoreInfo(url: string): Promise<Request> {
+    const response = await api.get(url);
+    const { id, types } = response.data;
+
+    return { id, types };
+  }
+
+  return (
+    <View>
+      {pokemons.map((pokemon) => (
+        <Text>{pokemon.name}</Text>
+      ))}
+    </View>
+  );
 }
